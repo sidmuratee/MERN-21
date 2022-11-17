fdbc699ec5527f489f8b98d3debd3206887c61c4
smurtee <muratees@yahoo.com>
2022-11-16 19:40:20 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const routes = require('./routes');
 require('dotenv')
 const app = express();
 const PORT = process.env.PORT || 3001;
+process.env.MONGO_DB
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
